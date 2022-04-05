76fc827471ccac997661a77f6affd178fe6560ca
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-05 23:24:26 +0200

    A commit

--- src/main/java/se/iths/crimedatabase/view/CriminalsThymeleafController.java
+++ src/main/java/se/iths/crimedatabase/view/CriminalsThymeleafController.java
@@ -7,20 +7,30 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import se.iths.crimedatabase.entity.Address;
+import se.iths.crimedatabase.entity.Crime;
 import se.iths.crimedatabase.entity.Criminal;
+import se.iths.crimedatabase.service.AddressService;
+import se.iths.crimedatabase.service.CrimeService;
 import se.iths.crimedatabase.service.CriminalService;
 
 @Controller
 public class CriminalsThymeleafController {
 
     private final CriminalService service;
+    private final CrimeService crimeService;
+    private final AddressService addressService;
 
-    @Autowired
-    public CriminalsThymeleafController(CriminalService service) {
+    public CriminalsThymeleafController(CriminalService service, CrimeService crimeService, AddressService addressService) {
         this.service = service;
+        this.crimeService = crimeService;
+        this.addressService = addressService;
     }
 
-   @GetMapping("/criminals")
+    @Autowired
+
+
+    @GetMapping("/criminals")
     public ModelAndView showCriminals() {
         ModelAndView mav = new ModelAndView("list-criminals");
         Iterable<Criminal> allCriminals = service.findAll();
@@ -31,7 +41,11 @@ public class CriminalsThymeleafController {
     @GetMapping("/addCriminalForm")
     public ModelAndView addCategoryForm() {
         ModelAndView mav = new ModelAndView("add-criminal-form");
+        Iterable<Crime> listCrimes = crimeService.findAll();
+        Iterable<Address> listAddresses = addressService.findAll();
         Criminal newCriminal = new Criminal();
+        mav.addObject("listAddresses", listAddresses);
+        mav.addObject("listCrimes", listCrimes);
         mav.addObject("criminal", newCriminal);
         return mav;
     }
