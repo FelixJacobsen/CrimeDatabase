522d0bb40dd77179a6d82bd2753296c5e5e14033
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-07 00:02:52 +0200

    Add all repository's and constructor

---
+++ src/main/java/se/iths/crimedatabase/util/ExampleData.java
@@ -0,0 +1,23 @@
+package se.iths.crimedatabase.util;
+
+import org.springframework.stereotype.Component;
+import se.iths.crimedatabase.repository.*;
+
+@Component
+public class ExampleData {
+
+    private AddressRepository addressRepository;
+    private VictimRepository victimRepository;
+    private CriminalRepository criminalRepository;
+    private CrimeRepository crimeRepository;
+    private CategoryRepository categoryRepository;
+
+    public ExampleData(AddressRepository addressRepository, VictimRepository victimRepository, CriminalRepository criminalRepository, CrimeRepository crimeRepository, CategoryRepository categoryRepository) {
+        this.addressRepository = addressRepository;
+        this.victimRepository = victimRepository;
+        this.criminalRepository = criminalRepository;
+        this.crimeRepository = crimeRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+}
